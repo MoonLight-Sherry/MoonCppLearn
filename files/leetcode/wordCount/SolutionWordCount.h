9e1b0996999cf72ad8b694efedce0804f42c2f43
CHINA\hjmin <hjmin@telenav.cn>
2021-10-25 11:03:58 +0800

    Jira Id: add 2 more solutions.

--- files/leetcode/wordCount/SolutionWordCount.h
+++ files/leetcode/wordCount/SolutionWordCount.h
@@ -1,12 +1,64 @@
-#pragma once
 #include <string>
+#include <regex>
+#include <sstream>
+#include <istream>
+
 using namespace std;
 
 
 class Solution
 {
 public:
-	int countValidWords(string sentence)
+	int countValidWords(string sentence) {
+        //return countValidWordsLoop(sentence);
+        //return countValidWordsReg(sentence);
+        return countValidWordsSimple(sentence);
+    }
+
+private:
+    
+    int countValidWordsSimple(string s) {
+        string w;
+        istringstream ss(s);
+        int ans = 0;
+        while (ss >> w) ans += valid(w);
+        return ans;
+    }
+    
+    bool valid(string &s) {
+        int hyphen = 0, N = s.size();
+        for (int i = 0; i < N; ++i) {
+            if (isdigit(s[i])) return false; // no digit
+            if (isalpha(s[i])) continue; // skip letters
+            if (s[i] == '-') {
+                if (++hyphen > 1) return false; // at most one hyphen allowed
+                if (i - 1 < 0 || !isalpha(s[i - 1]) || i + 1 >= N || !isalpha(s[i + 1])) return false; // hyphen must be surrounded by letters
+            } else if (i != N - 1) return false; // punctuation, if any, must be the last character of token
+        }
+        return true;
+    }
+    
+    int countValidWordsReg(string sentence){
+		int count = 0;
+		
+		// Defining the regex pattern
+		regex valid_word("[a-z]*([a-z]-[a-z])?[a-z]*[!,.]?");
+
+		// splitting the sentence to words
+		stringstream s(sentence);
+		string word;
+		while(getline(s, word, ' ')) {
+		
+			// Checking if the word matches the regex pattern
+			if(word != "" && regex_match(word, valid_word)){
+				++count;
+			} 
+		}
+
+		return count;
+    }
+
+    int countValidWordsLoop(string sentence)
 	{
 		printSentence(sentence);
 
@@ -75,12 +127,12 @@ public:
 private:
 	//Always has debug info output, and you can comment out them when submitting code
 	void printIsntWord(string sentence, int start, int end) {
-		printf("Isn't Word: %s. start:%d, end:%d\n\r", sentence.substr(start, end - start).c_str(), start, end);
+		//printf("Isn't Word: %s. start:%d, end:%d\n\r", sentence.substr(start, end - start).c_str(), start, end);
 	};
 	void printIsWord(string sentence, int start, int end) {
-		printf("Word: %s. start:%d, end:%d\n\r", sentence.substr(start, end - start).c_str(), start, end);
+		//printf("Word: %s. start:%d, end:%d\n\r", sentence.substr(start, end - start).c_str(), start, end);
 	};
 	void printSentence(string sentence) {
-		printf("sentence is: %s", sentence.c_str());
+		//printf("sentence is: %s", sentence.c_str());
 	};
 };
